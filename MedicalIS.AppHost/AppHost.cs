509636eb3bf80333d277299eefdbaaa03779c6af
Londongrad <Teslagradus@gmail.com>
2025-09-20 15:55:49 +0900

    Removed unused entities from tthe branch

--- MedicalIS.AppHost/AppHost.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var sqlite = builder.AddSqlite("sqlite");
-
-builder.AddProject<Projects.MedicalIS_WebApi>("medicalis-webapi")
-    .WithReference(sqlite);
-
-builder.Build().Run();
